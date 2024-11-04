85061550c6c18dafd75c9ad0315630ac08723ba0
stefanshaj <stefan.hudson@hotmail.com>
2024-11-04 10:57:35 -0300

    Criando projeto

---
+++ src/main/java/com/stefanhudson/desafio/gestor_projetos/GestorProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.stefanhudson.desafio.gestor_projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefanhudson/desafio/gestor_projetos/GestorProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanhudson.desafio.gestor_projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
